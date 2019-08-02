5355aa5d3f096b94030b965b05e905f90b95b15b
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-02 09:35:39 +0200

    Make sure that .screenshots folder is accessible by other processes

--- src/utils/commonSetup.js
+++ src/utils/commonSetup.js
@@ -3,7 +3,7 @@ const SCREENSHOTS_DIR_PATH = './.screenshots';
 
 function run(){
     if (!fs.existsSync(SCREENSHOTS_DIR_PATH)){
-        fs.mkdirSync(SCREENSHOTS_DIR_PATH);
+        fs.mkdirSync(SCREENSHOTS_DIR_PATH, 0o777);
     }
 }
 
